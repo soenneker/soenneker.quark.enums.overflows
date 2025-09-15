698cc6b5c30d7440d8f05704323b7a11a947c7bb
Jake Soenneker <jake@soenneker.com>
2025-09-15 16:57:40 -0500

    Initial commit

---
+++ src/Overflow.cs
@@ -0,0 +1,18 @@
+﻿using Intellenum;
+using Soenneker.Quark.Enums.GlobalKeywords;
+
+namespace Soenneker.Quark.Enums.Overflows;
+
+/// <summary>
+/// Typed values for the CSS overflow property in .NET enumeration form.
+/// </summary>
+[Intellenum<string>]
+public sealed partial class Overflow
+{
+    public static readonly Overflow Auto = new("auto");
+    public static readonly Overflow Hidden = new("hidden");
+    public static readonly Overflow Visible = new("visible");
+    public static readonly Overflow Scroll = new("scroll");
+
+    public static implicit operator Overflow(GlobalKeyword style) => new(style.Value);
+}
---
+++ test/Soenneker.Quark.Enums.Overflows.Tests/OverflowTests.cs
@@ -0,0 +1,13 @@
+﻿using Soenneker.Tests.Unit;
+using Xunit;
+
+namespace Soenneker.Quark.Enums.Overflows.Tests;
+
+public sealed class OverflowTests : UnitTest
+{
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
