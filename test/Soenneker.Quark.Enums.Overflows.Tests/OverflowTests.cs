47c7e5dffc509a09de24ed73cf8ff29b7d480192
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:02:29 -0500

    TUnit conversion

--- test/Soenneker.Quark.Enums.Overflows.Tests/OverflowTests.cs
+++ test/Soenneker.Quark.Enums.Overflows.Tests/OverflowTests.cs
@@ -1,11 +1,10 @@
 using Soenneker.Tests.Unit;
-using Xunit;
 
 namespace Soenneker.Quark.Enums.Overflows.Tests;
 
 public sealed class OverflowTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
